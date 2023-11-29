016135c7e411f9281992f06a9a956f8523599514
Hopi <89178015+Hopiboi@users.noreply.github.com>
2023-11-29 17:20:24 +0800

    Prototype

--- Assets/Scripts/Level Related/NextLevel.cs
+++ Assets/Scripts/Level Related/NextLevel.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
+        if(collision.tag == "Entrance")
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
